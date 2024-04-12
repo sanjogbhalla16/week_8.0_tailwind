35e874b8ecadece0077f138fcc338a391bc462a0
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-04-12 16:28:45 +0530

    tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
+import {VideoCard} from "../components/VideoCard"
 export default function Home() {
   return (
     <div>
-      hi there
+      <VideoCard></VideoCard>
     </div>
   );
 }
